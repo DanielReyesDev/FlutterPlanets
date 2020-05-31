83106c118ca164023417e9364cfc10078a664964
Daniel Reyes Sanchez <daniel.reyes@wizeline.com>
2020-05-31 12:09:27 -0500

    Minor fixes to compile and work with beta Channel: iOS, Android and Chrome

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/vn0e21f/Developer/Flutter/SDK"
+export "FLUTTER_APPLICATION_PATH=/Users/vn0e21f/Developer/Flutter/FlutterPlanets"
+export "FLUTTER_TARGET=/Users/vn0e21f/Developer/Flutter/FlutterPlanets/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/vn0e21f/Developer/Flutter/SDK/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
